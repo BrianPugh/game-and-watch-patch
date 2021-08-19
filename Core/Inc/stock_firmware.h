105c05b85027380997ebd9b2126a7082ed4e06eb
Brian Pugh <bnp117@gmail.com>
2021-08-18 20:58:19 -0700

    Add read_buttons findings

--- Core/Inc/stock_firmware.h
+++ Core/Inc/stock_firmware.h
@@ -1,3 +1,65 @@
 #pragma once
 
+#include <stdint.h>
+
+/**
+ *
+ */
 void (* const stock_Reset_Handler)(void) = 0x08017a45;
+
+
+/**
+ *
+ * Mapping
+ * Note: 
+ *   * if UP is pressed, it explicitly disables DOWN (UP gets priority)
+ *   * if RIGHT is pressed, it explicitly disables LEFT (RIGHT gets priority)
+ *
+ * Bit     Description
+ * -------------------
+ *   0     RIGHT
+ *   1     LEFT
+ *   2     DOWN
+ *   3     UP
+ *   4
+ *   5
+ *   6     B
+ *   7     A
+ *   8     TIME
+ *   9     PAUSE/SET
+ *   10    GAME
+ *   11
+ *   12
+ *   13
+ *   14
+ *   15
+ *   16
+ *   17
+ *   18
+ *   19
+ *   20
+ *   21
+ *   22
+ *   23
+ *   24
+ *   25
+ *   26
+ *   27
+ *   28
+ *   29
+ *   30
+ *   31
+ */
+typedef uint32_t gamepad_t;
+gamepad_t (* const stock_read_buttons)(void) = 0x08010d48;
+
+
+#define GAMEPAD_RIGHT ( 1 << 0 )
+#define GAMEPAD_LEFT  ( 1 << 1 )
+#define GAMEPAD_DOWN  ( 1 << 2 )
+#define GAMEPAD_UP    ( 1 << 3 )
+#define GAMEPAD_B     ( 1 << 6 )
+#define GAMEPAD_A     ( 1 << 7 )
+#define GAMEPAD_TIME  ( 1 << 8 )
+#define GAMEPAD_PAUSE ( 1 << 9 )
+#define GAMEPAD_GAME  ( 1 << 10 )
--- patches/patches.py
+++ patches/patches.py
@@ -10,9 +10,12 @@ class Patches(list):
     def append(self, *args, **kwargs):
         super().append(Patch(*args, **kwargs))
 
-def parse_patches(arg):
+def parse_patches(args):
     patches = Patches()
 
-    patches.append(0x4, "bootloader", message="Invoke custom bootloader prior to calling stock Reset_Handler")
+    patches.append(0x4, "bootloader",
+                   message="Invoke custom bootloader prior to calling stock Reset_Handler")
+    patches.append(0x6b52, "read_buttons",
+                   message="Intercept button presses for macros")
 
     return patches

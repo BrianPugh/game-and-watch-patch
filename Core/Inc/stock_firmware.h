ef5219e14e775bcdf417ef042e33295a52da16c5
Brian Pugh <bnp117@gmail.com>
2021-08-19 12:14:24 -0700

    clean up button macro

--- Core/Inc/stock_firmware.h
+++ Core/Inc/stock_firmware.h
@@ -35,33 +35,16 @@ void (* const stock_Reset_Handler)(void) = 0x08017a45;
  *   13
  *   14
  *   15
- *   16
- *   17
- *   18
- *   19
- *   20
- *   21
- *   22
- *   23
- *   24
- *   25
- *   26
- *   27
- *   28
- *   29
- *   30
- *   31
  */
-typedef uint32_t gamepad_t;
+typedef uint16_t gamepad_t;
 gamepad_t (* const stock_read_buttons)(void) = 0x08010d48 | THUMB;
 
-
-#define GAMEPAD_RIGHT ( 1 << 0 )
-#define GAMEPAD_LEFT  ( 1 << 1 )
-#define GAMEPAD_DOWN  ( 1 << 2 )
-#define GAMEPAD_UP    ( 1 << 3 )
-#define GAMEPAD_B     ( 1 << 6 )
-#define GAMEPAD_A     ( 1 << 7 )
-#define GAMEPAD_TIME  ( 1 << 8 )
-#define GAMEPAD_PAUSE ( 1 << 9 )
-#define GAMEPAD_GAME  ( 1 << 10 )
+#define GAMEPAD_RIGHT ((gamepad_t) ( 1 <<  0 ))
+#define GAMEPAD_LEFT  ((gamepad_t) ( 1 <<  1 ))
+#define GAMEPAD_DOWN  ((gamepad_t) ( 1 <<  2 ))
+#define GAMEPAD_UP    ((gamepad_t) ( 1 <<  3 ))
+#define GAMEPAD_B     ((gamepad_t) ( 1 <<  6 ))
+#define GAMEPAD_A     ((gamepad_t) ( 1 <<  7 ))
+#define GAMEPAD_TIME  ((gamepad_t) ( 1 <<  8 ))
+#define GAMEPAD_PAUSE ((gamepad_t) ( 1 <<  9 ))
+#define GAMEPAD_GAME  ((gamepad_t) ( 1 << 10 ))
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -4,8 +4,10 @@
 #include "cmsis_gcc.h"
 #include <assert.h>
 #include "gw_linker.h"
+#include "stm32h7xx_hal.h"
 
 
+#define BANK_2_ADDRESS 0x08100000
 #define BOOTLOADER_MAGIC 0x544F4F42  // "BOOT"
 #define BOOTLOADER_MAGIC_ADDRESS ((uint32_t *)0x2001FFF8)
 #define BOOTLOADER_JUMP_ADDRESS ((uint32_t **)0x2001FFFC)
@@ -14,6 +16,11 @@ static void  __attribute__((naked)) start_app(uint32_t pc, uint32_t sp) {
           msr msp, r1 /* load r1 into MSP */\n\
           bx r0       /* branch to the address at r0 */\n\
     ");
+}
+
+static inline void set_bootloader(uint32_t address){
+    *BOOTLOADER_MAGIC_ADDRESS = BOOTLOADER_MAGIC;
+    *BOOTLOADER_JUMP_ADDRESS = address;
 }
 
 /**
@@ -44,15 +51,25 @@ void bootloader(){
         *BOOTLOADER_MAGIC_ADDRESS = 0;
         uint32_t sp = (*BOOTLOADER_JUMP_ADDRESS)[0];
         uint32_t pc = (*BOOTLOADER_JUMP_ADDRESS)[1];
-        start_app(pc, sp);
+        start_app((uint32_t) pc, (uint32_t) sp);
     }
 
     start_app(stock_Reset_Handler, 0x20011330);
     while(1);
 }
 
+static void start_bank_2() {
+    set_bootloader(BANK_2_ADDRESS);
+    NVIC_SystemReset();
+}
+
 gamepad_t read_buttons() {
-    return stock_read_buttons();
+    gamepad_t gamepad = 0;
+    gamepad = stock_read_buttons();
+    if((gamepad & GAMEPAD_LEFT) && (gamepad & GAMEPAD_A) && (gamepad & GAMEPAD_GAME)){
+        start_bank_2();
+    }
+    return gamepad;
 }
 
 

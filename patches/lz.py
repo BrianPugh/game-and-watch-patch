7ecfcd08973b2ad9bead9204fb4715a98b66573a
Brian Pugh <bnp117@gmail.com>
2021-09-16 11:34:53 -0700

    add more details about the rwdata table format

--- patches/lz.py
+++ patches/lz.py
@@ -2,6 +2,17 @@
 def decompress(data):
     """ Decompresses rwdata used to initialize variables.
 
+    The table at address 0x0801807c has format:
+        0-3    Relative offset to this elements location to the initialization function.
+               Example:
+                    0x0801807c + (0xFFFE9617 - 0x100000000) == 0x8001693
+                    # 0x8001693 is the location of the bss_init_function
+        The function is then passed in a pointer to the offset 4.
+        The function returns a pointer to the end of the inputs it has consumed
+        from the table. For example, the lz_decompress function consumes 12 bytes
+        from the table. This, combined with the relative offset to the function,
+        means that 16 bytes in total of the table are used.
+
     Header format (not included in data) are 16 bytes in a table where:
 
     Index
@@ -9,7 +20,6 @@ def decompress(data):
     0-3    Relative offset of data from header
     4-7    Length of the compressed data in bytes
     8-11   Destination (in RAM) of decompressed data
-    12-15  Something else.
     """
 
     index = 0

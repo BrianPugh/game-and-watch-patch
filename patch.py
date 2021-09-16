2afa5d32026b926fd3a3cb6b3a2677219ed3e665
Brian Pugh <bnp117@gmail.com>
2021-09-15 21:27:07 -0700

    lz decompression for rw data

--- patch.py
+++ patch.py
@@ -16,6 +16,58 @@ from colorama import Fore, Back, Style
 colorama.init()
 
 
+def lz_decompress(data):
+    index = 0
+    out = bytearray()
+
+    while(index < len(data)):
+        opcode = data[index]
+        index += 1
+
+        # Opcode parsing
+        direct_len = opcode & 0x03
+        offset_256 = ((opcode >> 2) & 0x03)
+        pattern_len = opcode >> 4
+
+        if direct_len == 0:
+            direct_len = data[index] + 3
+            index += 1
+        assert direct_len > 0
+        direct_len -= 1
+
+        if pattern_len == 0xF:
+            pattern_len += data[index]
+            # pattern_len (not including the +2) can be in range [0, 270]
+            index += 1
+
+
+        ####
+        # Direct Copy
+        ####
+        for _ in range(direct_len):
+            out.append(data[index])
+            index += 1
+
+        #####
+        # Pattern
+        ####
+        if pattern_len > 0:
+            offset_add = data[index]
+            index += 1
+
+            if offset_256 == 0x03:
+                offset_256 = data[index]
+                index += 1
+
+            offset = offset_add + offset_256 * 256
+            # offset can be in range [0, 0xffff]
+
+            # +2 because anything shorter wouldn't be a pattern.
+            for _ in range(pattern_len + 2):
+                out.append(out[-offset])
+
+    return out
+
 class MissingSymbolError(Exception):
     """"""
 

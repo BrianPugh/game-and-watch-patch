d986892630161fd9f161f2438125ffa921f81a74
Brian Pugh <bnp117@gmail.com>
2021-09-29 10:04:24 -0700

    infrastructure for dynamic ips patching

---
+++ Core/Inc/ips.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+enum {
+    IPS_PATCH_OK = 0,
+    IPS_PATCH_WRONG_HEADER,
+};
+typedef uint8_t ips_patch_res_t;
+
+ips_patch_res_t ips_patch(uint8_t *dst, const uint8_t *src, const uint8_t *patch);

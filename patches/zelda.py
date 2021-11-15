d7cd5c8aaf86b963f295528823c8b585e014ffe2
Brian Pugh <bnp117@gmail.com>
2021-11-14 17:27:07 -0800

    zelda initial commit

---
+++ patches/zelda.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+
+from PIL import Image
+
+import patches
+
+from .compression import lzma_compress
+from .exception import BadImageError, InvalidStockRomError
+from .firmware import Device, ExtFirmware, Firmware, IntFirmware
+from .tileset import bytes_to_tilemap, tilemap_to_bytes
+from .utils import (
+    printd,
+    printe,
+    printi,
+    round_down_word,
+    round_up_page,
+    seconds_to_frames,
+)
+
+build_dir = Path("build")  # TODO: expose this properly or put in better location
+
+
+class ZeldaGnW(Device, name="zelda"):
+    class Int(IntFirmware):
+        STOCK_ROM_SHA1_HASH = "ac14bcea6e4ff68c88fd2302c021025a2fb47940"
+        STOCK_ROM_END = 0x1B6E0  # Used for generating linker script.
+        KEY_OFFSET = 0x16590
+        NONCE_OFFSET = 0x16594
+        RWDATA_OFFSET = 0x1B390
+        RWDATA_LEN = 20
+        RWDATA_DTCM_IDX = 0  # decompresses to 0x2000_A800
+
+    class Ext(ExtFirmware):
+        STOCK_ROM_SHA1_HASH = "1c1c0ed66d07324e560dcd9e86a322ec5e4c1e96"
+        # ENC_LEN = 0xF_E000
+
+        def _verify(self):
+            import ipdb
+
+            ipdb.set_trace()
+            h = self.hash(self[0x20000 : (0x3254A0 - 0x20000)])
+            if h != self.STOCK_ROM_SHA1_HASH:
+                raise InvalidStockRomError
+
+    class FreeMemory(Firmware):
+        FLASH_BASE = 0x240F2124
+        FLASH_LEN = 0  # 0x24100000 - FLASH_BASE
+
+    def argparse(self, parser):
+        self.args = parser.parse_args()
+        return self.args
+
+    def patch(self):
+        if not self.args.encrypt:
+            # Disable OTFDEC
+            self.internal.nop(0x16536, 2)
+            self.internal.nop(0x1653A, 1)
+
+        internal_remaining_free = len(self.internal) - self.int_pos
+        compressed_memory_free = (
+            len(self.compressed_memory) - self.compressed_memory_pos
+        )
+
+        return internal_remaining_free, compressed_memory_free
